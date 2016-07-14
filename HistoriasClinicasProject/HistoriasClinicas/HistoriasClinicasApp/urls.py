8b25d870e02fd87b33b2ea46edb892fcb4790976
John Villavicencio <jjvillavicencio@utpl.edu.ec>
2016-07-14 17:32:43 -0500

    Data tables

--- HistoriasClinicasProject/HistoriasClinicas/HistoriasClinicasApp/urls.py
+++ HistoriasClinicasProject/HistoriasClinicas/HistoriasClinicasApp/urls.py
@@ -5,4 +5,8 @@ url(r'^$', 'index_view', name='index'),
 url(r'^hisclidat', 'hisclidat', name='hisclidat'),
 url(r'^bushisclidat', 'bushisclidat', name='bushisclidat'),
 url(r'^template', 'template', name='template'),
+url(r'^pacientedat', 'pacientedat', name='pacientedat'),
+url(r'^buspacientedat', 'buspacientedat', name='buspacientedat'),
+url(r'^buscontactosdat', 'buscontactosdat', name='buscontactosdat'),
+url(r'^contactosdat', 'contactosdat', name='contactosdat'),
 )
--- HistoriasClinicasProject/HistoriasClinicas/HistoriasClinicasApp/views.py
+++ HistoriasClinicasProject/HistoriasClinicas/HistoriasClinicasApp/views.py
@@ -8,9 +8,19 @@ import datetime,json
 def index_view(request):
 	return render_to_response('index.html', context=RequestContext(request))
 def bushisclidat(request):
-	historias = HistoriaClinica.objects.all().values('his_codigo', 'pac_cedula', 'con_codigo', 'his_fecha_creacion', 'his_hora_creacion')
+	historias = HistoriaClinica.objects.all().values('his_codigo', 'pac_cedula', 'pac_cedula__pac_nombre', 'con_codigo__con_nombre', 'his_fecha_creacion', 'his_hora_creacion')
 	return HttpResponse(json.dumps(list(historias), cls=DjangoJSONEncoder), content_type = 'application/json')
 def hisclidat(request):
 	return render_to_response('hisclidat.html', context=RequestContext(request))
+def buspacientedat(request):
+	pacientes = Paciente.objects.all().values('pac_cedula', 'tip_codigo__tip_nombre', 'can_codigo__can_nombre', 'par_codigo__par_nombre', 'pac_codigo', 'pac_nombre', 'pac_fecha_nac', 'sexo', 'pac_direccion', 'pac_telefono')
+	return HttpResponse(json.dumps(list(pacientes), cls=DjangoJSONEncoder), content_type = 'application/json')
+def pacientedat(request):
+	return render_to_response('pacientedat.html', context=RequestContext(request))
+def buscontactosdat(request):
+	contactos = Contacto.objects.all().values('con_codigo', 'par_codigo__par_nombre', 'con_nombre', 'con_telefono', 'con_trabajo')
+	return HttpResponse(json.dumps(list(contactos), cls=DjangoJSONEncoder), content_type = 'application/json')
+def contactosdat(request):
+	return render_to_response('contactosdat.html', context=RequestContext(request))
 def template(request):
 	return render_to_response('template.html', context=RequestContext(request))
